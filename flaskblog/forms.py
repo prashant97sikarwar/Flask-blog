d1ce55254ae83a53a723f55935d4cfec629c9a70
prashant sikarwar <prashant97sikarwar@gmail.com>
2021-02-08 22:38:11 +0530

    user authentcation

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email,  EqualTo
+from wtforms.validators import DataRequired, Length, Email,  EqualTo, ValidationError
+from flaskblog.models import User
+
 
 #Registration form
 class RegistrationForm(FlaskForm):
@@ -14,6 +16,17 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')]) #confirm password can't be empty and has to be 
                                                                                                             #equal to password
     submit = SubmitField('Sign Up') #Submit button
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
     
 # Login form     
 class LoginForm(FlaskForm):
