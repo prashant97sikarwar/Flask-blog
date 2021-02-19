c6a20f00148fbdf1f6ff30ce45de574e37d336a4
prashant sikarwar <prashant97sikarwar@gmail.com>
2021-02-19 14:03:33 +0530

    email verification

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -62,3 +62,19 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+    
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first')
+        
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password',validators=[DataRequired()]) #password can't be empty
+    
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')]) #confirm password can't be empty and has to be 
+                                                                                                            #equal to password
+    submit = SubmitField('Reset Password')
